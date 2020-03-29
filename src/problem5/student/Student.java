56575e5bc43df0c8feb0570e714e27cf79f9e7ff
Saurabh Patel <patelsaurabh501@gmail.com>
2020-03-29 22:22:49 +0530

    Update Student.java
    student class created

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -6,5 +6,52 @@
  */
 package problem5.student;
 // to store student information and properties
+
 public class Student {
+
+  private int rollNumber;
+  private String Name;
+  private int backLogCounter;
+  private int appearingCounter;
+  
+  
+    public Student(int rollNumber,String name,int backLogCounter,int appearingCounter){
+    this.rollNumber=rollNumber;
+    Name=name;
+    this.backLogCounter=backLogCounter;
+    this.appearingCounter=appearingCounter;
+    }
+    public int getRollNumber(){
+     return rollNumber;
+    }
+    
+    public String getName(){
+     return Name;
+    }
+    
+    public int getBackLogCounter(){
+     return backLogCounter;
+    }
+    
+    public int getAppearingCounter(){
+     return appearingCounter;
+    }
+
+    
+    public void setRollNumber(int rollNumber){
+     this.rollNumber=rollNumber;
+    }
+    
+    public void setName(String name){
+     Name=name;
+    }
+
+    public void setBackLogCounter(int backLogCounter){
+     this.backLogCounter=backLogCounter;
+    }
+    public void setAppearingCounter(int appearingCounter){
+     this.appearingCounter=appearingCounter;
+    }
+    public String toString(){ 
+    return "Student rollnumber- "+rollNumber+",name- "+Name+",backlogs- "+backLogCounter+",appearing- "+appearingCounter;
 }
