1b8c70decfbcd450f2eccd956e0521a5c1f08cd0
Saurabh Patel <patelsaurabh501@gmail.com>
2020-03-29 22:05:38 +0530

    Update MyMain.java
    main class

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -6,6 +6,38 @@
  */
 package problem4.main;
 // executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
 public class MyMain {
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        MyQueue que = new MyQueue();
+        TreeNode root = new TreeNode(10);
+
+        bst.insert(root, 50);
+        bst.insert(root, 205);
+        bst.insert(root, 150);
+        bst.insert(root, 190);
+        bst.insert(root, 97);
+        bst.insert(root, 62);
+        bst.insert(root, 300);
+        bst.insert(root, 135);
+        bst.insert(root, 90);
+        bst.insert(root, 45);
+        bst.insert(root, 62);
+
+        que.preOrder(root);
+        que.display(que);
+
+        System.out.print("Enter to find preorder Successor : ");
+        que.printsuccessor(sc.nextInt());
+
+    }
+
 }
