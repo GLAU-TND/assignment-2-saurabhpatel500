39a6eee9946915c374387cfc69e5474ae2f13d54
Saurabh Patel <patelsaurabh501@gmail.com>
2020-03-29 22:26:41 +0530

    Update MyMain.java
    main class

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -7,4 +7,19 @@
 package problem5.main;
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        MyCircularQueue obj = new MyCircularQueue();
+        obj.isEmpty();
+        obj.enqueue(1,"happy",2,5);
+        obj.enqueue(2,"raja",2,6);
+        obj.enqueue(3,"lala",1,5);
+        obj.enqueue(4,"hero",2,7);
+        obj.display();
+        obj.size();
+        obj.dequeue();
+        obj.display();
+        obj.size();
+        obj.isEmpty();
+
+    }
 }
