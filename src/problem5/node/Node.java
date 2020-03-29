92ebf0c55a17cd897caf7ba00127b0e3c8e77c84
Saurabh Patel <patelsaurabh501@gmail.com>
2020-03-29 22:24:23 +0530

    Update Node.java
    node class created

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -6,5 +6,30 @@
  */
 package problem5.node;
 // to define node properties
+import problem5.student.Student;
+
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Student student){
+      this.student=student;
+      next=null;
+    }
+
+    public Student getData(){
+      return student;
+    }
+
+    public void setData(Student student){
+      this.student=student;
+    }
+    
+    public Node getNext(){
+      return next;
+    }
+    
+    public void setNext(Node next){
+      this.next=next;
+    }
 }
